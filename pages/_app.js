4ac2eb4eb95d3fdb057046b21ad55a13cc9d4858
nbhaohao <nbhaohao@163.com>
2021-01-21 20:21:21 +0800

    config:增加 TS, scss 支持

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import Link from "next/link";
 
 export default function Home() {
