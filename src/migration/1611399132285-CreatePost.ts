aead4d2a73d8068c6bd3af3eb57a993933bddca5
nbhaohao <nbhaohao@163.com>
2021-01-24 10:42:52 +0800

    config:搭建 typeORM 开发配置

---
+++ src/migration/1611399132285-CreatePost.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreatePost1611399132285 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.createTable(
+      new Table({
+        name: "posts",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "title",
+            type: "varchar",
+          },
+          {
+            name: "content",
+            type: "text",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.dropTable("posts");
+  }
+}
