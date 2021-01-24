f33125248b9bffe509f898b5fb6af74c7473203c
nbhaohao <nbhaohao@163.com>
2021-01-24 16:55:56 +0800

    feat:新增 CreateUsersTable migration 文件

--- src/seed.ts
+++ src/seed.ts
@@ -3,7 +3,6 @@ import { createConnection } from "typeorm";
 
 createConnection()
   .then(async (connection) => {
-    console.log(connection);
     connection.close();
   })
   .catch((error) => console.log(error));
